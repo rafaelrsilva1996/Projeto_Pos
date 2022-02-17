65c5a46fcacd303a73a00da8ea57485a00f3f25f
Rafael <rafael.r.silva1996@gmail.com>
2022-02-17 20:03:59 -0300

    Finalizando instalaçao do Bootstrap

--- server/server/settings.py
+++ server/server/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'crispy_forms',
     
     'seriados',
 ]
@@ -130,3 +132,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
